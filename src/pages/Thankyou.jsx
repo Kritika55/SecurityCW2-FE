fa1f419056cfbd75ae2ef60ca290f908c599bb6b
Kritika55 <94155352+Kritika55@users.noreply.github.com>
2024-08-11 13:43:45 +0545

    last page error handling

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -10,7 +10,7 @@ const ThankYou = () => {
           <h2 className="text-3xl font-bold mb-2">Thank You!</h2>
           <AiOutlineCheckCircle className="text-4xl text-green-500 mb-2" />
         </div>
-        <p className="text-lg mb-4">Your reservation has been confirmed.</p>
+        <p className="text-lg mb-4">Your order has been confirmed.</p>
         <p className="text-lg mb-4">We appreciate your business and look forward to serving you again.</p>
         <Link
           to="/"
